d7ec3ca44f959d7a6256f66044cbfc027cadb3c9
Gabriele <lele9.2@hotmail.it>
2014-07-05 15:08:57 +0200

    corrette chiamate al db

--- turing/app/controllers/docente_controller.rb
+++ turing/app/controllers/docente_controller.rb
@@ -13,21 +13,22 @@ class DocenteController < ApplicationController
 
     s = ""
     (0..1).step(1) do |column|
-      old = Teacher.order(:surname).all[teachers * column].surname[0]
+      old = Teacher.order(:surname).pluck(:surname)[teachers * column][0]
       s += "<div class='column'>"
       (0..teachers - 1).step(1) do |n|
-        if docente = Teacher.order(:surname).all[teachers * column + n]
-          current = docente.surname[0]
+        if docente_id = Teacher.order(:surname).pluck(:id)[teachers * column + n]
+          surname = Teacher.find_by_id(docente_id).surname
+          current = surname[0]
           if current != old
             s += "<br />"
             old = current
           end
 
-          url = docente.surname.downcase.split(" ").join("_")
+          url = surname.downcase.split(" ").join("_")
           s += "<a href='/docente/" + url + "'>"
-          s += docente.surname
+          s += surname
           s += " "
-          s += docente.name
+          s += Teacher.find_by_id(docente_id).name
           s += "</a><br />"
         end
       end
