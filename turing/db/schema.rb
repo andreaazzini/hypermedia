4344f7cd8836bc97b8152a6a7a4cf325e41a9f24
andreaazzini <andrea2.azzini@mail.polimi.it>
2014-07-06 03:21:07 +0200

    Fine Single Topic School

--- turing/db/schema.rb
+++ turing/db/schema.rb
@@ -115,9 +115,4 @@ ActiveRecord::Schema.define(version: 20140705202803) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
