0f43f2185e47933db0181761e73d9dabe5f03a02
Gabriele <lele9.2@hotmail.it>
2014-07-06 17:24:05 +0200

    inizio implementazione attività extra

--- turing/db/schema.rb
+++ turing/db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140706130234) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "activities_teachers", id: false, force: true do |t|
