0ac29c6fa75321cf45bfa5af981cc7d6b509990b
Gabriele <lele9.2@hotmail.it>
2014-07-06 17:49:47 +0200

    rimosse migration errate

--- turing/db/migrate/20140627202559_create_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTable < ActiveRecord::Migration
-  def change
-    create_join_table :teachers, :school_classes do |t|
-      t.index :teacher_id
-      t.index :school_class_id
-    end
-  end
-end
--- turing/db/migrate/20140627204025_add_subject_in_teachers_school_classes.rb
+++
@@ -1,6 +0,0 @@
-class AddSubjectInTeachersSchoolClasses < ActiveRecord::Migration
-  def change
-    add_column :school_classes_teachers, :subject, :string
-    add_column :school_classes_teachers, :coordinator, :integer
-  end
-end
