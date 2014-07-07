5729c24d63d5642c1acd07004df1e92548c700c0
andreaazzini <andrea2.azzini@mail.polimi.it>
2014-07-07 21:49:12 +0200

    Eliminate migrate inutili

--- turing/db/migrate/20140707131253_remove_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUser < ActiveRecord::Migration
-  def change
-    drop_table :users
-  end
-end
--- turing/db/migrate/20140707154652_remove_user2.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUser2 < ActiveRecord::Migration
-  def change
-    drop_table :users
-  end
-end
